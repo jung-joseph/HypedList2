2ed29fcd24d67df4b59ac3ba6975ad1fa7447f62
Joseph Jung <joseph.jung@icloud.com>
2020-12-16 10:04:04 -0700

    Initial Commit

---
+++ HypedList2/HypedList2App.swift
@@ -0,0 +1,17 @@
+//
+//  HypedList2App.swift
+//  HypedList2
+//
+//  Created by Joseph Jung on 12/16/20.
+//
+
+import SwiftUI
+
+@main
+struct HypedList2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
